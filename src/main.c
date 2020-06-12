1abfb82ffcb9c5d3a1a3c96ef731a1ca3425bd7c
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-06-12 18:23:45 +0200

    hello world from lua

--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+#include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, const char* argv)
+int main(int argc, char** argv)
 {
-  printf("hello world!\n");
-  return 0;
+  lua_State* L = luaL_newstate();
+  luaL_openlibs(L);
+
+  if (luaL_dofile(L, "lua/main.lua") == 0)
+  {
+    return 0;
+  }
+  else
+  {
+    const char *err = luaL_checkstring(L, -1);
+    printf("Failed to run main.lua: %s\n", err);
+    return 1;
+  }
 }
